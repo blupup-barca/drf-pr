8088eb7da496112fe4dc989c11be5838e92bccb0
sasha chernikova <chernikova.sahsa@yandex.ru>
2025-11-03 00:42:04 +0300

    hw 30.2

--- users/serializers.py
+++ users/serializers.py
@@ -1,9 +1,13 @@
 from rest_framework import serializers
-from users.models import User, Payments
+
+from users.models import Payments, User
+
+
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = "__all__"
+
 
 class PaymentsSerializer(serializers.ModelSerializer):
 
--- users/views.py
+++ users/views.py
@@ -1,10 +1,12 @@
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import generics
 from rest_framework.filters import OrderingFilter
-from users.models import User, Payments
-from users.serializers import PaymentsSerializer
-from users.serializers import UserSerializer
 from rest_framework.generics import CreateAPIView
+
+from users.models import Payments, User
+from users.serializers import PaymentsSerializer, UserSerializer
+
+
 class PaymentsListAPIView(generics.ListAPIView):
     queryset = Payments.objects.all()
     serializer_class = PaymentsSerializer
@@ -12,9 +14,11 @@ class PaymentsListAPIView(generics.ListAPIView):
     filterset_fields = ("course", "lessons")
     ordering_fields = ("date", "payment_method")
 
+
 class UserCreateApiView(CreateAPIView):
     serializer_class = UserSerializer
     queryset = User.objects.all()
+
 
 class PaymentCreateAPIView(CreateAPIView):
     serializer_class = PaymentsSerializer
